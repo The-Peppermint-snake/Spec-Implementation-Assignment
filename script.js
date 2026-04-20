421edb016433565f3948b3b070bc1afbee4d9863
Beatrice <brkovalik@gmail.com>
2026-04-20 13:52:27 -0400

    i'm so cooked

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
+//TO-DO
+    //make grid from html
+    //add event isners
+        //on update
+            //grid x&y
+            //start x&y
+            //goal x&y
+        //on click
+            //walls -> true
+    //change colors of td objects
+        //walls -> black
+        //visited -> yellow
+        //pathtaken -> orange
+        //goal -> red
+        //start ->green
+    //BFS function
+
 //from HTML
-const gridX = document.getElementById("gridX");
-const gridY = document.getElementById("gridY");
+const gridXHTML = parseInt(document.getElementById("gridX"),10);
+const gridYHTML = parseInt(document.getElementById("gridY"),10);
+const startXHTML = parseInt(document.getElementById("startX"),10);
+const startYHTML = parseInt(document.getElementById("startY"),10);
+const goalXHTML = parseInt(document.getElementById("goalX"),10);
+const goalYHTML = parseInt(document.getElementById("goalY"),10);
 const table = document.getElementById("table");
+
+//array of array of node objects
+let cells = []
 
 class Node {
     constructor(x,y,element,neighbors) {
@@ -13,19 +37,49 @@ class Node {
         this.visted = false;
         this.pathTaken = false;
     }
+    changeColor() {
+        if(this.wall == true) {
+            //change color to black
+        }
+        if(this.visted == true) {
+            //change color to yellow
+        }
+        if(this.pathTaken == true) {
+            //change color to orange
+        }
+    }
+}
+
+class Algorithm {
+    constructor(walk) {
+        this.start = cells[startXHTML,startYHTML];
+        this.goal = cells[goalXHTML,goalYHTML];
+        this.walk = walk;
+    }
+}
+
+
+BFS = new Algorithm(BFSFunc())
+
+function BFSFunc(s,g) {
+
 }
 
 function makeGrid(gridX,gridY) {
+    console.log(1)
         for(let y = 0; y < gridY; y++) {
-            let tableRow = table.insertRow(-1)
-            console.log(y);
+            let tableRow = table.insertRow(-1);
+            let rows = [];
+            console.log(2)
             for(let x = 0; x < gridX; x++) {
-                
-                let node = new Node(x,y)
+                let node = new Node(x,y);
                 node.element = tableRow.insertCell();
-                console.log(node.element)
+                rows.push(node);
+                console.log(3)
             }
+            cells.push(rows);
         }
 }
 
-makeGrid(4,4)
+console.log(4)
+makeGrid(gridXHTML,gridXHTML);
