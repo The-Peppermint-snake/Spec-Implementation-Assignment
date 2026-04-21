78ab1567a78f3406f321f5a6441a59aabfe9b6f7
Beatrice <brkovalik@gmail.com>
2026-04-21 14:02:59 -0400

    added event listners

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 //TO-DO
-    //make grid from html
     //add event isners
-        //on update
-            //grid x&y
-            //start x&y
-            //goal x&y
         //on click
             //walls -> true
     //change colors of td objects
@@ -13,19 +8,43 @@
         //pathtaken -> orange
         //goal -> red
         //start ->green
+    //get neighbors method
     //BFS function
 
-//from HTML
-const gridXHTML = parseInt(document.getElementById("gridX"),10);
-const gridYHTML = parseInt(document.getElementById("gridY"),10);
-//const startXHTML = parseInt(document.getElementById("startX"),10);
-//const startYHTML = parseInt(document.getElementById("startY"),10);
-const goalXHTML = parseInt(document.getElementById("goalX"),10);
-const goalYHTML = parseInt(document.getElementById("goalY"),10);
+const gridXHTML = document.getElementById("gridX");
+const gridYHTML = document.getElementById("gridY");
+const startXHTML = document.getElementById("startY");
+const startYHTML = document.getElementById("startX");
+const goalXHTML = document.getElementById("goalY");
+const goalYHTML = document.getElementById("goalX");
 const table = document.getElementById("table");
 
-//array of array of node objects
-let cells = []
+gridXHTML.addEventListener("change", function() {
+    let gridepls = makeGrid(gridXHTML.value,gridYHTML.value,table);
+
+})
+gridYHTML.addEventListener("change", function() {
+    let gridepls = makeGrid(gridXHTML.value,gridYHTML.value,table);
+
+})
+startXHTML.addEventListener("change", function() {
+    console.log("start x")
+    let gridepls = makeGrid(gridXHTML.value,gridYHTML.value,table);
+    BFS = new Algorithm(BFSFunc(), gridepls)
+
+})
+startYHTML.addEventListener("change", function() {
+    let gridepls = makeGrid(gridXHTML.value,gridYHTML.value,table);
+    BFS = new Algorithm(BFSFunc(), gridepls)
+})
+goalXHTML.addEventListener("change", function() {
+    let gridepls = makeGrid(gridXHTML.value,gridYHTML.value,table);
+    BFS = new Algorithm(BFSFunc(), gridepls)
+})
+goalYHTML.addEventListener("change", function() {
+    let gridepls = makeGrid(gridXHTML.value,gridYHTML.value,table);
+    BFS = new Algorithm(BFSFunc(), gridepls)
+})
 
 class Node {
     constructor(x,y,element,neighbors) {
@@ -49,38 +68,42 @@ class Node {
             //change color to orange
         }
     }
+
+    getNeighbors() {
+        //get neighbors of the object
+        // -1 and +1 from x and y
+    }
 }
 
-//class Algorithm {
-    //constructor(walk) {
-        //this.start = cells[startXHTML][startYHTML];
-        //this.goal = cells[goalXHTML][goalYHTML];
-        //this.walk = walk;
-    //}
-//}
-
-
-//BFS = new Algorithm(BFSFunc())
-
-//function BFSFunc(s,g) {
-//}
-
-function makeGrid(gridX,gridY) {
-    console.log(1)
-        for(let y = 0; y < gridY; y++) {
-            let tableRow = table.insertRow(-1);
-            let rows = [];
-            console.log(2)
-            for(let x = 0; x < gridX; x++) {
-                let node = new Node(x,y);
-                node.element = tableRow.insertCell();
-                rows.push(node);
-                console.log(3)
-            }
-            cells.push(rows);
+function makeGrid(gridX,gridY,table) {
+    let cells = [];
+    table.innerHTML = "";
+    for(let y = 0; y < gridY; y++) {
+        let tableRow = table.insertRow(-1);
+        let rows = [];
+        for(let x = 0; x < gridX; x++) {
+            let node = new Node(x,y);
+            node.element = tableRow.insertCell();
+            rows.push(node);
         }
+        cells.push(rows);
+    }
+    return cells;
+}
+let gridepls = makeGrid(gridXHTML.value,gridYHTML.value,table);
+
+class Algorithm {
+    constructor(walk, cells) {
+        this.start = cells[startXHTML.value-1][startYHTML.value-1].element;
+        this.goal = cells[goalXHTML.value-1][goalYHTML.value-1].element;
+        this.walk = walk;
+
+    }
 }
 
-console.log(gridXHTML)
-console.log(gridYHTML)
-makeGrid(gridXHTML,gridYHTML);
+
+BFS = new Algorithm(BFSFunc(), gridepls)
+
+function BFSFunc(s,g) {
+
+}
